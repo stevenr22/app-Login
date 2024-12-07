994669cd5bbe33e0ca090e18bd69d91b6cded737
stevenr22 <rojasguerreros185@gmail.com>
2024-12-07 17:50:00 -0500

    NUEVO PROYECTO

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+    <h2>HOLA MUNDO</h2>
+    </>
+  )
+}
+
+export default App
